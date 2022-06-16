684eab2865e96fd2db99b55001b1b3d6c4f00249
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-06-16 12:57:45 -0500

    commit

--- week3.js
+++ week3.js
@@ -106,3 +106,56 @@ let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(total100(array1));
 
 //Write a function that takes an array of numbers and returns the average of all the elements in the array.
+let myArray = [4, 10, 12, 3]
+let sum1 = myArray.reduce (function(accumulator, currentValue) {
+    return accumulator + currentValue;
+});
+console.log(sum1);
+let avg1 = sum1 / myArray.length;
+console.log (avg1);
+
+//Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+
+let array3 = [100, 200, 300, 400];
+let array4 = [200, 300, 100];
+let sumArray3 = array3.reduce (function(accumulator, currentValue) {
+    return accumulator + currentValue;
+});
+console.log (sumArray3);
+let sumArray4 = array4.reduce (function(accumulator, currentValue) {
+    return accumulator + currentValue;
+});
+console.log (sumArray4);
+let avg3 = sumArray3 / array3.length;
+let avg4 = sumArray4 / array4.length;
+if (avg3 > avg4) {
+    console.log (true);
+} else {
+    console.log(false);
+}
+
+//Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+var isHotOutside = true
+var moneyInPocket = 20
+
+function willBuyDrink (isHotOutside, moneyInPocket) {
+    if (isHotOutside = true && moneyInPocket > 10.50) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(willBuyDrink(true, 20));
+
+//Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
+
+let daysSinceWater = 3
+function shouldWater (daysSinceWater) {
+    if (daysSinceWater >= 5) {
+        console.log("It's been " + daysSinceWater + " since you have watered your plants. Water plants now!");
+    } else {
+        console.log("It's been " + daysSinceWater + "since you watered your plants");
+    }
+}
+
+//This function allows me to get notice of when I need to water my plants as I am terrible at remembering.
