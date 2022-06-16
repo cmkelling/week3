abf6a93bba7098977126924103df056fa14205bc
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-06-16 09:21:57 -0500

    commit on 'Main'

--- week3.js
+++ week3.js
@@ -14,4 +14,19 @@ ageDifference (lastElement, firstElement);
 ages.push(26);
 console.log(ages);
 
-ageDifference(lastElement, firstElement);
+var lastElement = ages.pop();
+var firstElement = ages.shift();
+
+console.log(ages);
+difference (lastElement, firstElement);
+
+//Use a loop to iterate through the array and calculate the average age. Print the result to the console.
+var totalAge = 0
+for (let i = 0; i < ages.length; i++) {
+    totalAge += ages[i]
+}
+
+var avg = total / ages.length;
+
+console.log(avg);
+
